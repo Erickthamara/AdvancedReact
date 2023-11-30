fb5455e2d0fa539c0059b02fa3f41b404a8f1e07
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-30 09:55:33 +0300

    Used useState to render some data and buttons to delete each individual item. Note that buttons cannot directly call functions that require data. removeOnePerson(0).They need to be wrapped.

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,49 @@
+import { data } from "../../../data.js";
+import { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeAllPeople = () => {
+    setPeople([]);
+  };
+  const removeOnePerson = (id) => {
+    console.log(id);
+    const newList = people.filter((people) => {
+      return people.id !== id;
+    });
+    setPeople(newList);
+  };
+
+  return (
+    <>
+      <h1>useState array example</h1>
+      <div>
+        {people.map((person) => {
+          const { name, id } = person;
+          return (
+            <div key={id}>
+              <h3>{name}</h3>
+              <button
+                type="button"
+                className="btn"
+                onClick={() => removeOnePerson(id)}
+              >
+                DELETE
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={removeAllPeople}
+      >
+        Clear All ITEMS
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
