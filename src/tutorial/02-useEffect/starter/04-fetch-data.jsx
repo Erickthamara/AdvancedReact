dcccce89c834253918a03da6a075edac6a285adc
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-12-07 12:04:09 +0300

    Created a list of github users with useEffect and useState

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,37 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const peopleData = async () => {
+      try {
+        const data = await fetch(url);
+        const people = await data.json();
+        setUsers(people);
+      } catch (error) {
+        console.error(`You have this error: ${error}`);
+      }
+    };
+    peopleData();
+  }, []);
+  //console.log(`This is the data ${users}`);
+
+  return (
+    <div>
+      <h2>{}</h2>
+      {users.map((oneUser) => {
+        const { login, url, avatar_url, id } = oneUser;
+        return (
+          <div key={id}>
+            <img src={avatar_url} alt="" />
+            <h2>{login}</h2>
+            <a href={url}>Profile</a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default FetchData;
