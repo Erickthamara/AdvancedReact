48b580ddb157999ea035f6675a0053c11eb05c83
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-12-13 10:34:43 +0300

    Whn adding functonalities to useEffect in components which are toggled,you need to add a cleanup function to prevent them from runninga fter you have unmounted the component.

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [name, setName] = useState("");
+  const toggleName = () => {
+    name ? setName("") : setName("Ricko");
+  };
+
+  return (
+    <div>
+      <h1>Cleanup Function</h1>
+      <button type="button" className="btn" onClick={toggleName}>
+        TOGGLE
+      </button>
+      {!name ? <h2>{`What is your name?`}</h2> : <DisplayName name={name} />}
+    </div>
+  );
+};
+
+const DisplayName = (props) => {
+  const { name } = props;
+  useEffect(() => {
+    console.log("Display name has run for the firts time");
+    /*  const id = setInterval(() => {
+      console.log("Hello Interval");
+    }, 1000); */
+    const someFunc = () => {
+      //some function
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => {
+      window.removeEventListener("scroll", someFunc);
+    };
+  }, []);
+
+  return <h2>My name is: {name}</h2>;
 };
 
 export default CleanupFunction;
